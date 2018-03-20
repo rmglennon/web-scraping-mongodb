26d205d2446a3503ee65d57022e5d7ff8c92dc18
Rhonda Glennon <rhondaglennon@gmail.com>
2018-03-19 18:31:19 -0700

    refactor and clean up front end code

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
+  $(document).on("click", ".save-btn", saveArticle);
+  $(document).on("click", ".scrape-new", scrapeArticles);
+  $(document).on("click", ".unsave-btn", removeFromSaved);
+  $(document).on("click", ".note-modal-btn", createNotesModal);
+  $(document).on("click", ".note-save-btn", saveNote);
+
+
   createPage();
 
   // load the page initially with articles
@@ -11,7 +18,7 @@ $(document).ready(function() {
   };
 
   // when the save button is clicked, get the article ID and set its saved property to true
-  $(".save-btn").on("click", function(event) {
+  function saveArticle(event) {
 
     var newSavedArticle = $(this).data();
     newSavedArticle.saved = true;
@@ -24,19 +31,20 @@ $(document).ready(function() {
         location.reload();
       }
     );
-  });
+  };
 
-  $(".scrape-new").on("click", function(event) {
+  function scrapeArticles(event) {
     // event.preventDefault();
     $.get("/scrape").then(
       function(data) {
         createPage();
       }
     );
-  });
+  };
 
   // when the button to removed a saved article from the saved list, get the article ID and set its saved property back to false
-  $(".unsave-btn").on("click", function(event) {
+
+  function removeFromSaved(event) {
     var newUnsavedArticle = $(this).data();
     newUnsavedArticle.saved = false;
     $.ajax("/saved/" + newUnsavedArticle.id, {
@@ -47,10 +55,11 @@ $(document).ready(function() {
         location.reload();
       }
     );
-  });
+  };
 
   // when the add note button is clicked on the saved articles page, show a modal and save the note into the note collection
-  $(".note-modal-btn").on("click", function(event) {
+
+  function createNotesModal(event) {
     var activeArticle = $(this).data();
     $.get("/notes/" + activeArticle.id).then(
       function(data) {
@@ -61,9 +70,9 @@ $(document).ready(function() {
         $("#note-modal-title").text("Notes for article: " + data.title);
         var noteItem;
         var noteDeleteBtn;
-        for (var i = 0; i < data.notes.length; i ++) {
+        for (var i = 0; i < data.notes.length; i++) {
           noteItem = $("<li>").text(data.notes[i].body);
-        //  noteItem.data("id", data.notes[i]._id);
+          //  noteItem.data("id", data.notes[i]._id);
           noteDeleteBtn = $("<button> Delete </button>").addClass("btn btn-danger delete-note-modal");
           noteDeleteBtn.attr("id", data.notes[i]._id);
           noteItem.prepend(noteDeleteBtn);
@@ -73,42 +82,42 @@ $(document).ready(function() {
     );
 
     $("#add-note-modal").modal("toggle");
-  });
+  };
 
-  $(".note-save-btn").on("click", function(event) {
+  function saveNote(event) {
 
     event.preventDefault();
     var newNote = {
       body: $("#note-body").val().trim()
     }
-  //  console.log(newNote);
+    //  console.log(newNote);
     $.ajax("/submit", {
       type: "POST",
       data: newNote
     }).then(
       function(data) {
-      location.reload();
-//console.log("notes data ", data);
+        window.location.reload(true);
+        //console.log("notes data ", data);
       }
     );
-  });
-  
-    $(".notes-list").on("click", ".delete-note-modal", function(event) {
-    console.log("delete was clicked");
-      var activeNote = $(this.id);
-     console.log(activeNote);
+  };
 
-      $.ajax("/notes/" +      ObjectId.fromString(activeNote), {
-        type: "DELETE"
-      }).then(
-        function(data) {
-          //location.reload();
-          console.log(data);
-          // use the article title from the response as the heading
+  $(".notes-list").on("click", ".delete-note-modal", function(event) {
+    //console.log("delete was clicked");
+    // var activeNote = $(this);
+    console.log($(this).attr("id"));
 
-      //    var test = data.notes[0].body;
-      //    console.log(test);
-    })
+    $.ajax("/notes/" + $(this).attr("id"), {
+      type: "DELETE"
+    }).then(
+      function(data) {
+        location.reload();
+        console.log(data);
+        // use the article title from the response as the heading
+
+        //    var test = data.notes[0].body;
+        //    console.log(test);
+      })
   });
 
 });
